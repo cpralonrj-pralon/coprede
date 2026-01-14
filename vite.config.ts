fe80425f85097b1dcb0dbf05eb377c853b0e6b75
Claudio <copredeiaia@gmail.com>
2026-01-14 18:04:50 -0300

    fix: deploy05

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,9 @@ export default defineConfig(({ mode }) => {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
     },
+    build: {
+      outDir: 'dist_prod'
+    },
     resolve: {
       alias: {
         '@': path.resolve(process.cwd(), '.'),
